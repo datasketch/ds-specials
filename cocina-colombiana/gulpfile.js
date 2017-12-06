2ce7c9b63dc3e6ed5ed6a9d9c3b88f0f148650f2
David Daza <03dazal@gmail.com>
2017-12-06 14:03:42 -0500

    Main banner a header (cocina-colombiana)

--- cocina-colombiana/gulpfile.js
+++ cocina-colombiana/gulpfile.js
@@ -1,21 +1,33 @@
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
 const gulp = require('gulp');
-const postcss = require('gulp-postcss');
-const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require('autoprefixer');
+const less = require('gulp-less');
 const livereload = require('gulp-livereload');
+const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('autoprefixer', function() {
-	return gulp.src('./src/css/*.css')
+	return gulp.src('./src/css/*.less')
 			.pipe(sourcemaps.init())
-			.pipe(postcss([autoprefixer()]))
+			.pipe(less())
+			.pipe(autoprefixer())
 			.pipe(sourcemaps.write('.'))
 			.pipe(gulp.dest('./assets/css'))
 			.pipe(livereload());
 })
 
-gulp.task('watch', function() {
-	livereload.listen();
-	gulp.watch('./src/css/*.css', ['autoprefixer']);
+gulp.task('babel', function() {
+	return gulp.src('./src/js/*.js')
+			.pipe(sourcemaps.init())
+			.pipe(babel())
+			.pipe(sourcemaps.write())
+			.pipe(gulp.dest('./assets/js'))
+			.pipe(livereload());
 })
 
-gulp.task('default', ['autoprefixer']);
+gulp.task('watch', function() {
+	livereload.listen();
+	gulp.watch('./src/css/*.less', ['autoprefixer']);
+	gulp.watch('./src/js/*.js', ['babel']);
+})
+
+gulp.task('default', ['autoprefixer', 'babel', 'watch']);
