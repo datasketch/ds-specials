0a040b4cb40fb6343983b010b4bc5a739eebdb3c
David Daza <03dazal@gmail.com>
2017-12-07 15:35:34 -0500

    Overlay menu

--- cocina-colombiana/src/js/index.js
+++ cocina-colombiana/src/js/index.js
@@ -1,13 +1,9 @@
 const nav = document.querySelector('.nav');
-
-function affix() {
-	const scrollValue = window.scrollY;
-	if (scrollValue > 100) {
-		nav.classList.add('scrolled');
-	}
-	if (scrollValue === 0) {
+console.dir(nav)
+const affix = () => {
+	window.scrollY > (window.innerHeight - nav.clientHeight) ? 
+		nav.classList.add('scrolled') :
 		nav.classList.remove('scrolled');	
-	}
 }
 
 affix();
--- cocina-colombiana/src/js/shared.js
+++ cocina-colombiana/src/js/shared.js
@@ -1,4 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const body = document.body;
 hamburger.addEventListener('click', e => {
 	hamburger.classList.toggle('is-active');
+	menu.classList.toggle('overlayed');
+	body.classList.toggle('no-scroll');
 });
