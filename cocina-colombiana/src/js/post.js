462f02b27b85c2bd68855bf435655216218b5b7a
David Daza <03dazal@gmail.com>
2017-12-19 10:29:24 -0500

    Add search field in glossary

--- cocina-colombiana/src/js/post.js
+++ cocina-colombiana/src/js/post.js
@@ -8,4 +8,16 @@ const margin = () => {
 (function() {
 	margin();
 	window.addEventListener('resize', margin);
+	if (window.List) {
+		const warning = document.querySelector('.no-results');
+		const options = { valueNames: ['ingredient-name'] };
+		const glossary = new List('glossary', options);
+		glossary.on('searchComplete', function(results) {
+			if (!results.visibleItems.length) {
+				warning.style.display = 'block';
+			} else {
+				warning.style.display = 'none';
+			}
+		})
+	}
 })();
