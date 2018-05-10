a2aa73598fcaefe04f7ec825d983be7830017124
David Daza <03dazal@gmail.com>
2018-05-09 19:01:15 -0500

    Fix fonts, add disqus script

---
+++ contratos-colombia/src/js/disqus.js
@@ -0,0 +1,11 @@
+const disqus_config = function() {
+  this.page.url = window.location.href; 
+  this.page.identifier = window.location.href;
+  this.language = "es_MX";
+};
+(function() {
+  const s = document.createElement('script');
+  s.src = '//datasketch.disqus.com/embed.js';
+  s.setAttribute('data-timestamp', +new Date());
+  document.body.appendChild(s)
+})();
