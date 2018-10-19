6f3e4783dbbcaaef3802575a844994bdf49e7369
David Daza <03dazal@gmail.com>
2018-10-19 08:48:32 -0500

    Add new repo (actores-educacion)

---
+++ actores-educacion/components/Organizations.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="organizations">
+    <div
+      v-for="(organization, index) in values"
+      :key="index"
+      class="organization"
+      @click="onclick(organization)"
+    >
+      <p class="organization__name">{{ organization.name }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Organizations',
+  props: {
+    values: {
+      type: Array,
+      required: true
+    },
+    onclick: {
+      type: Function,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style scoped>
+.organization {
+  cursor: pointer;
+  opacity: 1;
+  padding: 0 10px;
+  transition-delay: 5s;
+  transition: 0.15s ease-in-out;
+}
+
+.organization:hover {
+  background: #eeeeee;
+}
+
+.organization__name {
+  border-bottom: 1px solid #eeeeee;
+  font-size: 18px;
+  padding: 5px 0;
+}
+</style>
---
+++ actores-educacion/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <Map
+    :center="[4.6097100, -74.0817500]"
+    tile="https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}{r}.png" />
+</template>
+
+<script>
+import Map from '~/components/Map'
+
+export default {
+  components: {
+    Map
+  },
+  async fetch ({ store }) {
+    await store.dispatch('getSpreadsheetData')
+  }
+}
+</script>
