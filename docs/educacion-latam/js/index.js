591bb70cb0c99f05dc9d72d1d67f0efdaacc2711
David Daza <03dazal@gmail.com>
2018-12-17 09:00:54 -0500

    Build site (educacion-latam)

--- docs/educacion-latam/js/index.js
+++ docs/educacion-latam/js/index.js
@@ -1,10 +1,12 @@
-const header = document.querySelector('header')
+// const header = document.querySelector('header')
 
-window.addEventListener('scroll', function () {
-  const y = window.scrollY
-  if (y >= 100) {
-    header.classList.add('affixed')
-  } else {
-    header.classList.remove('affixed')
-  }
-})
+// window.addEventListener('scroll', function () {
+//   const y = window.scrollY
+//   if (y >= 100) {
+//     header.classList.add('affixed')
+//   } else {
+//     header.classList.remove('affixed')
+//   }
+// })
+
+'use strict';var header=document.querySelector('header');window.addEventListener('scroll',function(){var a=window.scrollY;100<=a?header.classList.add('affixed'):header.classList.remove('affixed')});
