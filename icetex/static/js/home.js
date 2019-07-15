5c21f7af330a8074e066ca21d29713ef9860d754
David Daza <03dazal@gmail.com>
2019-07-15 11:28:16 -0500

    Add interactivity to bullets

--- icetex/static/js/home.js
+++ icetex/static/js/home.js
@@ -4,7 +4,11 @@ const tabs = new Tabs(
   document.querySelectorAll('.tab'),
   document.querySelectorAll('.tab-content'),
 );
-const bullets = new Bullets(document.getElementById('bullets-container'), 'sesiones');
+const bullets = new Bullets({
+  bullets: document.getElementById('bullets-container'),
+  buttons: document.getElementById('bullets-buttons'),
+  info: document.getElementById('bullets-info'),
+});
 
 tabs.registerHandler();
 bullets.init();
