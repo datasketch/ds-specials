c835541229b7283a8deeefc11d48690256ecebe5
David Daza <03dazal@gmail.com>
2019-08-13 17:04:30 -0500

    the milky way

---
+++ via-lactea/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        barlow: ['"Barlow Condensed"', 'sans-serif']
+      },
+      textColor: {
+        'mine-shaft': '#333333'
+      },
+      backgroundColor: {
+        'mine-shaft': '#333333'
+      },
+      fontSize: {
+        '7xl': '5rem',
+        '8xl': '6rem'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
