1420e740af69c0cabf6d26e683bbc5f634315405
David Daza <03dazal@gmail.com>
2020-01-24 16:25:35 -0500

    elecciones y contratos on steroids

---
+++ elecciones-y-contratos/tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        denim: '#137fc0',
+        ecstasy: '#fa8223',
+        flamingo: '#e85a1e',
+        sunshade: '#FFA624',
+        tarawera: '#0a446b',
+        vermilion: '#f54205',
+        'fuchsia-pink': '#c250c2',
+        'moon-raker': '#ccc3f4',
+        'my-sin': '#fdb731',
+        'outer-space': '#213133',
+        'violet-eggplant': '#941994',
+        'wild-sand': '#f5f5f5'
+      },
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif']
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
